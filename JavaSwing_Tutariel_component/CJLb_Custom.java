3281e1e189a6b0b708e633d0d48448f96562997f
sadik <19358480+sadik-fattah@users.noreply.github.com>
2023-07-01 13:44:45 +0100

    change custom lbl

--- JavaSwing_Tutariel_component/CJLb_Custom.java
+++ JavaSwing_Tutariel_component/CJLb_Custom.java
@@ -1,20 +1,37 @@
 import javax.swing.*;
-import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.*;
+
 public class CJLb_Custom  extends JLabel {
+    private Color color;
+
+    private int rectWidth;
+
+    private int rectHeight;
+
+    private int offsetX;
+
     public CJLb_Custom() {
-        this("");
     }
 
-    public CJLb_Custom(String text) {
+    public CJLb_Custom(String text, Color color, int offsetX, int rectWidth, int rectHeight) {
         super(text);
+        this.color = color;
+        this.rectWidth = rectWidth;
+        this.rectHeight = rectHeight;
+        this.offsetX = offsetX;
+        setPreferredSize(new Dimension(rectWidth + offsetX, rectHeight));
     }
 
     public void paint(Graphics g) {
-        Rectangle r;
         super.paint(g);
-        r = g.getClipBounds();
-        g.drawLine(0, r.height - getFontMetrics(getFont()).getDescent(), getFontMetrics(getFont())
-                .stringWidth(getText()), r.height - getFontMetrics(getFont()).getDescent());
+        g.setColor(color);
+        g.fillRect(offsetX, 0, rectWidth + offsetX, rectHeight);
+    }
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(new CJLb_Custom("Test", Color.BLUE, 40, 40, 40));
+        f.pack();
+        f.setVisible(true);
     }
 }
